947ae75d9934b8c119fbed2ab0e3f20f0075cdcb
Atharva Pardeshi <atharva.exe@gmail.com>
2025-01-08 19:44:20 +0530

    chore: add env variables

--- src/env.mjs
+++ src/env.mjs
@@ -5,10 +5,14 @@ export const env = createEnv({
     LANGFLOW_APPLICATION_TOKEN: z.string().min(1),
     LANGFLOW_FLOW_ID: z.string().min(1),
     LANGFLOW_ID: z.string().min(1),
+    ASTRA_DB_TOKEN: z.string().min(1),
+    ASTRA_DB_URL: z.string().min(1),
   },
   runtimeEnv: {
     LANGFLOW_APPLICATION_TOKEN: process.env.LANGFLOW_APPLICATION_TOKEN,
     LANGFLOW_FLOW_ID: process.env.LANGFLOW_FLOW_ID,
     LANGFLOW_ID: process.env.LANGFLOW_ID,
+    ASTRA_DB_TOKEN: process.env.ASTRA_DB_TOKEN,
+    ASTRA_DB_URL: process.env.ASTRA_DB_URL,
   },
 })
